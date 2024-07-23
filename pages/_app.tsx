2929e677cb1554bf1c297f15675dbebe6da3c100
Stanley Pang <stanley.pang@mail.utoronto.ca>
2024-07-23 15:55:01 -0400

    implemented modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import "@/styles/globals.css";
 
 import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
+import Modal from "@/components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <>
+      <Modal actionLabel="Submit" isOpen title="Test Modal"/>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+      </>
   )
 }
